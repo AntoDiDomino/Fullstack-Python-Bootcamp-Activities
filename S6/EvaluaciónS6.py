83aaa5fc319fa28b22259f10336a635cc79bf334
Antonia Véliz <antodidomino@gmail.com>
2024-09-09 17:49:48 -0300

    git push *

--- S6/EvaluaciónS6.py
+++ S6/EvaluaciónS6.py
@@ -1,4 +1,16 @@
-num1 = 0
-num2 = 0
-num3 = 0
+num1 = int(input("Ingrese el primer número:\n"))
+num2 = int(input("Ingrese el segundo número:\n"))
+num3 = int(input("Ingrese el tercer número:\n"))
 
+if num1 <= num2 <= num3:
+    print(num1, num2, num3)
+elif num1 <= num3 <= num2:
+    print(num1, num3, num2)
+elif num2 <= num1 <= num3:
+    print(num2, num1, num3)
+elif num2 <= num3 <= num1:
+    print(num2, num3, num1)
+elif num3 <= num1 <= num2:
+    print(num3, num1, num2)
+else:
+    print(num3, num2, num1)
